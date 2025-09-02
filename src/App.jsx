2e9cdf709bf133157fde8e2ccd2de6c04ef2aa12
Sushil Kumar <kumarsushil6238@gmail.com>
2025-09-02 17:52:28 +0530

    Added Navbar and Footer to App,jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
             Courses, Login, ya Signup component yahan dikhega. */}
         <Outlet />
       </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer style={{ backgroundColor: '#333', color: '#fff', padding: '20px', textAlign: 'center' }}>
+      <p>&copy; 2025 EduApp. All rights reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
